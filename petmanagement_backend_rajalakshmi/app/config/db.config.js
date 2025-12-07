e204c7adaa54773fd6dfa59fa329cfde903b338f
flavin <flavin@flavins-Mac-mini.local>
2025-12-07 22:28:37 +0530

    new update

--- petmanagement_backend_rajalakshmi/app/config/db.config.js
+++ petmanagement_backend_rajalakshmi/app/config/db.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  url: "mongodb://localhost:27017/petmanagement"
+  url: "mongodb+srv://nexvyon_db_user:IZYb1PpuIDgi5jdh@cluster0.epvhnm9.mongodb.net/petmanagement"
 };
+
--- petmanagement_backend_rajalakshmi/server.js
+++ petmanagement_backend_rajalakshmi/server.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(cors(
   {
-    origin: ['http://localhost:4200', 'http://localhost:4200/'],
+    origin: ['http://localhost:4000', 'http://localhost:4200/'],
     credentials: true,
 
   }
